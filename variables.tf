d1a5bce10b4d3baf035f57833ade34cbbf13b8f6
aman sachdeva <asamansachdeva@gmail.com>
2022-10-19 03:57:00 +0530

    git lfs added

--- variables.tf
+++ variables.tf
@@ -1,28 +1,3 @@
-variable "aws_region" {
-       description = "The AWS region to create things into" 
-       default     = "us-east-1" 
-}
-
-variable "key_name" { 
-    description = " SSH keys to connect to ec2 instance" 
-    default     =  "ec2_example" 
-}
-
-variable "instance_type" { 
-    description = "instance type for ec2" 
-    default     =  "t2.micro" 
-}
-
-variable "security_group" { 
-    description = "Name of security group" 
-    default     = "ec2_scatha_sg" 
-}
-
-variable "tag_name" { 
-    description = "Tag Name of for Ec2 instance" 
-    default     = "my_ec2_scatha_instance" 
-} 
-variable "ami_id" { 
-    description = "AMI for Ubuntu Ec2 instance" 
-    default     = "ami-08c40ec9ead489470" 
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:46da11127b8c16f4ef8fe83b6d6909537ca7a6f10d06f7ad3644c61122ee2ac2
+size 691
